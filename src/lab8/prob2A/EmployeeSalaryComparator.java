35cf5a893bfbba492bccae2485ed580bac317094
Vorleak Chy <vorleak.chy@gmail.com>
2018-12-11 14:18:55 -0600

    Init lab8.prob2A

---
+++ src/lab8/prob2A/EmployeeSalaryComparator.java
@@ -0,0 +1,15 @@
+package lab8.prob2A;
+
+import java.util.Comparator;
+
+public class EmployeeSalaryComparator implements Comparator<Employee> {
+	@Override
+	public int compare(Employee e1, Employee e2) {
+		if (e1.salary == e2.salary)
+			return 0;
+		else if (e1.salary < e2.salary)
+			return -1;
+		else
+			return 1;
+	}
+}
