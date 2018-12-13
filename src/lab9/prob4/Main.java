23a8b1aa4aa6e29d287557ae2b0a2bcce92faa7e
Vorleak Chy <vorleak.chy@gmail.com>
2018-12-12 20:44:37 -0600

    Add lab9 prob4

---
+++ src/lab9/prob4/Main.java
@@ -0,0 +1,37 @@
+package lab9.prob4;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class Main {
+
+	public static void main(String[] args) {
+		List<Set<String>> sets = new ArrayList<>();
+		
+		Set<String> set1 = new HashSet<>();
+		set1.addAll(Arrays.asList("A", "B"));
+		
+		Set<String> set2 = new HashSet<>();
+		set2.add("D");
+		
+		Set<String> set3 = new HashSet<>();
+		set3.addAll(Arrays.asList("1", "3", "5"));
+		
+		sets.addAll(Arrays.asList(set1, set2, set3));
+		
+		Set<String> output = union(sets);
+		output.forEach(System.out::println);
+	}
+	
+	public static Set<String> union(List<Set<String>> sets) {
+		final Set<String> initialSet = new HashSet<String>();
+		
+		return sets.stream()
+			.reduce(initialSet, (set1, set2) -> { set1.addAll(set2); return set1;} );
+	}
+
+}
