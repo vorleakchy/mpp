dd9c1cf3c117e3c4eab6dfecc18fb4c71c469f98
Vorleak Chy <vorleak.chy@gmail.com>
2018-12-14 21:27:31 -0600

    Add solution for lab10 prob8

--- src/lab10/prob8/Queue.java
+++ src/lab10/prob8/Queue.java
@@ -9,14 +9,14 @@ public class Queue {
 	}
 	private Node head;
 	private Node tail;
-	public void add(Object newValue) {
+	synchronized public void add(Object newValue) {
 		Node n = new Node();
 		if(head == null) head = n;
 		else tail.next = n;
 		tail = n;
 		tail.value = newValue;
 	}
-	public Object remove() {
+	synchronized Object remove() {
 		if(head == null) return null;
 		Node n = head;
 		head = n.next;
