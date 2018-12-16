2b69574df8fb599b986acfe47edd38ff357ca981
Vorleak Chy <vorleak.chy@gmail.com>
2018-12-16 14:51:33 -0600

    Refactor

--- src/lab9/prob1/partB/Main.java
+++ src/lab9/prob1/partB/Main.java
@@ -1,16 +1,16 @@
 package lab9.prob1.partB;
 
+import java.util.Arrays;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class Main {
 
 	public static void main(String[] args) {
-		Stream<String> names = Stream.of("Bill", "Thomas", "Mary");
-		
-		String output = names.collect(Collectors.joining(", "));
-		
-		System.out.println(output);
+		System.out.println(
+				Arrays.asList("Bill", "Thomas", "Mary")
+					.stream()
+					.collect(Collectors.joining(", ")));
 	}
 
 }
